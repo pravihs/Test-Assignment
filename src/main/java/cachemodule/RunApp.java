0fe9799ec37915d6784fc33248706e06b6e6639d
praveen <pravihs@gmail.com>
2024-03-15 11:43:22 +0100

    update build gradle for run

--- src/main/java/cachemodule/RunApp.java
+++ src/main/java/cachemodule/RunApp.java
@@ -1,11 +1,11 @@
 package main.java.cachemodule;
 
-public class run {
+public class RunApp {
     public static void main(String[] args) {
         Cache<Integer, String> cache = new Cache<>(2, EvictionStrategy.LEAST_RECENTLY_USED);
         cache.put(1, "One");
         cache.put(2, "Two");
-        System.out.println(cache.get(1)); // Output: One
+        System.out.println(cache.get(1));
         cache.put(3, "Three"); // Evicts key 2
         System.out.println(cache.get(2)); // Output: null
         System.out.println(cache.get(3)); // Output: Three
